6329cd356f6e6b3a4528a83768c0813ed6925eae
sandy010503 <sundeepsandy2003@gmail.com>
2024-10-17 15:14:15 +0530

    Very First commit

---
+++ myappDevOpsDemo/myappDevOpsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myappDevOpsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
